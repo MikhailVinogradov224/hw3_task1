30a4015bc84a8c7ee3550f6a0d7934cf9178c41b
MikhailVinogradov224 <m89119750476@gmail.com>
2022-04-24 00:38:38 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿    int x, a, x2 = 0;
+    Console.Write("Введите пятизначное число: ");
+    string s = Console.ReadLine();     
+    if (int.TryParse(s, out x))
+    {
+      if (s[0]>99999 && s[0]<10000)Console.WriteLine("не пятизначное число");
+        else
+            {
+             a = x;
+             while (a > 0)
+                {
+                    x2 *= 10;
+                    x2 += a % 10;
+                    a /= 10;
+            }
+            if (x == x2) Console.WriteLine("палиндром");
+            else Console.WriteLine("не палиндром");
+        }
+    }
+    else
+     {
+        Console.WriteLine("Введено не число");
+    }
